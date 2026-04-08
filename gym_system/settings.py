81b300266408895d1856d1e16590a5f96aadccb0
Samuel Atima <[200~2404905@students.kcau.ac.ke>
2026-04-08 22:41:56 +0300

    Updated settings for Render deployment

--- gym_system/settings.py
+++ gym_system/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-ezllzfoyzg8&^znmc6uojag1+er*#aw^55)^q6-uksxv(bq!ra
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
@@ -115,4 +113,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
